355fb3385c5fd98f2980f37e48d583af6b60021f
Igor Barbashov <bis200705@gmail.com>
2021-12-04 16:10:36 +0300

    refactor: add koa-bodyparser middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 const { rootRouter } = require('./resources/root/root.router');
 const { usersRouter } = require('./resources/users/user.router');
 
 const app = new Koa();
+
+app.use(bodyParser());
 
 app.use(rootRouter.routes());
 app.use(rootRouter.allowedMethods());
